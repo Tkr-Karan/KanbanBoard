7f6085bb92d82a457df5a63f72c49c9d0b018b60
Tkr-Karan <karankumar072508@gmail.com>
2023-06-27 02:21:45 +0530

    adding ticket dynamically

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let addBtn = document.querySelector(".add-btn");
 let modal = document.querySelector(".modal-cont");
+let allPriorityColor = document.querySelectorAll(".priority-color");
+let textAreaContent = document.querySelector(".textarea-cont");
+let mainContent = document.querySelector(".main-cont");
+
+
 let addModal = true;
 
 addBtn.addEventListener("click", function(){    
@@ -12,3 +17,47 @@ addBtn.addEventListener("click", function(){
     }
 
 })
+
+
+// Setting the marker
+for(let i = 0; i < allPriorityColor.length;  i++){
+    allPriorityColor[i].addEventListener("click", function(){
+        for(let j = 0; j < allPriorityColor.length; j++){
+            if(allPriorityColor[j].classList.contains("active")){
+                allPriorityColor[j].classList.remove("active");
+            }
+        }
+        allPriorityColor[i].classList.add("active");
+    })
+
+}
+
+textAreaContent.addEventListener("keydown", function(e){
+    if(e.key === "Enter"){
+        textAreaContent.value = "";
+        console.log("Enter Clicked!!!!")
+        createTicket();
+        modal.style.display = "none";
+        addModal = true
+    }
+})
+
+function createTicket(){
+    // <div class="ticket-cont">
+    //         <div class="ticket-color blue"></div>
+    //         <div class="ticket-id">#saefwe</div>
+    //         <div class="ticket-area">Some Task</div>
+    // </div>
+
+    let ticketCont = document.createElement("div");
+    ticketCont.setAttribute("class", "ticket-cont");
+
+    console.log(ticketCont);
+    ticketCont.innerHTML = `<div class="ticket-color blue"></div>
+                             <div class="ticket-id">#saefwe</div>
+                             <div class="ticket-area">Some Task</div> 
+                            `
+
+    mainContent.append(ticketCont);
+
+}
