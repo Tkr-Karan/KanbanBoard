2951e5c52b2d73c72f00be0b7dcdaa6de259a0cc
Tkr-Karan <karankumar072508@gmail.com>
2023-07-01 22:59:23 +0530

    Adding LocalStorage functionality

--- script.js
+++ script.js
@@ -4,16 +4,33 @@ let allPriorityColor = document.querySelectorAll(".priority-color");
 let textAreaContent = document.querySelector(".textarea-cont");
 let mainContent = document.querySelector(".main-cont");
 let removebtn = document.querySelector(".rmv-btn");
+let priorityColor = document.querySelectorAll(".toolbox-priority-cont div");
 
 let modalPriorityColor = "red";
 
 let color = ["red", "blue", "green", "black"];
+
+let ticketArr = [];
 
 var uid = new ShortUniqueId();
 
 let addModal = true;
 let removeFlag = false;
 let lockFlag = false;
+
+
+if(localStorage.getItem("tickets")){
+    console.log(localStorage.getItem("tickets"))
+    let strArr = localStorage.getItem("tickets");
+    console.log(strArr);
+    let tickArr = JSON.parse(strArr);
+
+    for(let i = 0;i < tickArr.length; i++){
+        console.log(tickArr[i])
+        let ticket = tickArr[i];
+        createTicket(ticket.task, ticket.color, ticket.id); 
+    }
+}
 
 addBtn.addEventListener("click", function(){    
     if(addModal){
@@ -25,6 +42,37 @@ addBtn.addEventListener("click", function(){
     }
     
 })
+
+
+// priority color selection
+for(let i = 0; i < priorityColor.length; i++){
+    priorityColor[i].addEventListener("click", function(){
+        // console.log(priorityColor[i].classList[1]);
+
+        let currentPriorityColor = priorityColor[i].classList[1];
+
+        let allTicketColor = document.querySelectorAll(".ticket-color")
+        for(let j = 0; j < allTicketColor.length; j++){
+            // console.log(allTicketColor[j]);
+            let currentTicketColor = allTicketColor[j].classList[1];
+            // console.log(currentTicketColor);
+            if(currentPriorityColor == currentTicketColor){
+                console.log(currentPriorityColor, currentTicketColor)
+                allTicketColor[j].parentElement.style.display = "block";
+            }else{
+                allTicketColor[j].parentElement.style.display = "none";
+            }
+        }
+
+        priorityColor[i].addEventListener("dblclick", function(){
+            for(let j = 0; j < allTicketColor.length; j++){
+                allTicketColor[j].parentElement.style.display = "block";
+
+            }
+        })
+    })
+
+}
 
 
 // Setting the marker
@@ -54,19 +102,27 @@ textAreaContent.addEventListener("keydown", function(e){
         let textVal = e.target.value;
         textAreaContent.value = "";
         // console.log("Enter Clicked!!!!")
-        createTicket(textVal);
+        createTicket(textVal, modalPriorityColor);
         modal.style.display = "none";
         addModal = true
     }
 })
 
-function createTicket(text){
+function createTicket(text, modalPriorityColor, ticketId){
     // <div class="ticket-cont">
     //         <div class="ticket-color blue"></div>
     //         <div class="ticket-id">#saefwe</div>
     //         <div class="ticket-area">Some Task</div>
     // </div>
-    let id = uid();
+    // let id = uid();
+
+    let id;
+
+    if(ticketId){
+        id = ticketId;
+    }else{
+        id = uid();
+    }
     
     let ticketCont = document.createElement("div");
     ticketCont.setAttribute("class", "ticket-cont");
@@ -81,10 +137,23 @@ function createTicket(text){
     `
     
     mainContent.append(ticketCont);
+    ticketArr.push({id : id, color : modalPriorityColor, task: text});
+
+    updateLocalStorage();   
+
+    // console.log(ticketArr);
+
+
     
     ticketCont.addEventListener("click", function(){
         if(removeFlag){
             ticketCont.remove();
+            let idx = ticketArr.findIndex(function(obj){
+                return obj.id = id;
+            })
+
+            ticketArr.splice(idx, 1);
+            updateLocalStorage();
         }
     })
     
@@ -103,19 +172,32 @@ function createTicket(text){
             //     if(color[i] == currentColor){
                 //         currentColorIndex = i;
                 //         break;
-                                            //     }
-                                            // }
-                                            
-                                            // console.log(currentColorIndex);
-                                            
-                                            let nextColorIndex = (currentColorIndex+1)%color.length;
-                                            let nextColor = color[nextColorIndex];
-                                            
-                                            // console.log(nextColor);
-                                            
-                                            ticketColor.classList.remove(currentColor);
-                                            ticketColor.classList.add(nextColor);
-                                        })
+            //     }
+            // }
+            
+            // console.log(currentColorIndex);
+            
+            let nextColorIndex = (currentColorIndex+1)%color.length;
+            let nextColor = color[nextColorIndex];
+            
+            // console.log(nextColor);
+            
+            ticketColor.classList.remove(currentColor);
+            ticketColor.classList.add(nextColor);
+
+            let idx;
+            for(let i = 0; i < ticketArr.length; i++){
+                if(ticketArr[i].id == id){
+                    idx = i;
+                    break;
+                }
+            }
+            console.log(idx);
+            ticketArr[idx].color = nextColor;
+            console.log(ticketArr);
+
+            updateLocalStorage();
+        })
                                         
     // lock and unlock event listener
     let lockUnlock = ticketCont.querySelector(".lock-unlock i");
@@ -131,6 +213,22 @@ function createTicket(text){
             lockUnlock.classList.add("fa-lock");
             taskArea.setAttribute("contenteditable", "false");
         }
+
+        // let idx;
+        // for(let i = 0; i < ticketArr.length; i++){
+        //     if(ticketArr[i].id == id){
+        //         idx = i;
+        //         break;
+        //     }
+        // }
+
+        let idx = ticketArr.findIndex(function(obj){
+            return obj.id = id;
+        })
+
+        ticketArr[idx].task = taskArea.innerText;
+
+        updateLocalStorage();
     })
     
 }
@@ -148,3 +246,8 @@ removebtn.addEventListener("click", function(){
     }
 })
 
+
+function updateLocalStorage(){
+    let strArr = JSON.stringify(ticketArr);
+    localStorage.setItem("tickets", strArr);
+}
